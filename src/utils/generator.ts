247b4e26e8cb20abea42923dd0eec4882242aca1
feige996 <1020102647@qq.com>
2025-09-20 11:43:48 +0800

    refactor: 移除模板导入的.js扩展名以保持一致性

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 import fs from 'fs-extra'
-import { TemplateHandler } from './template.js'
+import { TemplateHandler } from './template'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
